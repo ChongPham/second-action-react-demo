58dfe10eb4ec9607bb0472ca31dbd838d885f416
trongpq091999@gmail.com <trongpq091999@gmail.com>
2026-08-07 00:25:01 +0700

    using js action + S3 to deploy static webapp

--- .github/actions/sec8-s3-js-action/index.js
+++ .github/actions/sec8-s3-js-action/index.js
@@ -1,8 +1,16 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+// import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
 function run() {
+    // 1) get some input values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+
+    // 2) upload files
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     core.notice('Hello from my custom Javascript Action!');
 }
 
